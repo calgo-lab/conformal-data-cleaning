e878d6f5eac7e06cded9cf4601c1bd34839cc196
chandlerNick <njchandler2002@gmail.com>
2026-01-20 14:52:18 +0100

    Update __init__.py

--- src/conformal_data_cleaning/cleaner/__init__.py
+++ src/conformal_data_cleaning/cleaner/__init__.py
@@ -1,5 +1,6 @@
 from .conformal_forest import ConformalForestCleaner
 from .forest import ForestCleaner
 from .precision_forest import PrecisionForestCleaner
+from ._base import BaseCleaner
 
-__all__ = ["ConformalForestCleaner", "ForestCleaner", "PrecisionForestCleaner"]
+__all__ = ["ConformalForestCleaner", "ForestCleaner", "PrecisionForestCleaner", "BaseCleaner"]
